7e3fb21084c4ceec8c55446b731fea042a1550ad
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2018-03-03 09:49:42 +0100

    Test done

--- test/test.js
+++ test/test.js
@@ -1,34 +1,58 @@
-// assert es la libreria que nos permite verificar cosas
+//
+// Testing support:
+//
+// - mocha ...: https://mochajs.org/
+// - assert ..: https://nodejs.org/api/all.html
+//
 const assert = require('assert');
-// instanciamos el codigo de nuestra libreria
 const tumejortorrent_scraper = require('../lib/tumejortorrent');
+const url = require("url");
 
-// describe es la forma en la que describimos que pasara
+describe('tumejortorrent', function () {
+    describe('#parseVideoPremieres()', function () {
 
-describe('testeo', function () {
-    describe('#testeo', function () {
-        it('Should return kaka', function () { // no done
-            data = tumejortorrent_scraper.test('mola');
-            expect(data).to.equal('hola mola');
-
-        });
-    });
-});
-
-describe('parseFilms', function () {
-    describe('#parseFilms()', function () {
-        it('Should return list url', function () { // no done
-
-            return tumejortorrent_scraper.parseFilms('http://tumejortorrent.com/peliculas-x264-mkv/').then(function (urlList) {
-                expect(urlList.length).to.equal(10);
+        it('should return a list with at least one valid url', function () {
+            return tumejortorrent_scraper.parseVideoPremieres().then(function (urlList) {
+                urlList.forEach(function (element) {
+                    assert.ok(url.parse(element));
+                    //console.log('URL: ' + element);
+                });
+                assert.ok(urlList.length > 1);
+                console.log('urlList size:' + urlList.length);
             });
-
-            /*
-            return shapeshift.getRate(pair).then(function (data) {
-                expect(data.pair).to.equal(pair);
-                expect(data.rate).to.have.length(400);
-            }); // no catch, it'll figure it out since the promise is rejected
-       */
         });
     });
+
+    describe('#parseBillboardFilms()', function () {
+
+        it('should return a list with at least one valid url', function () {
+            return tumejortorrent_scraper.parseBillboardFilms().then(function (urlList) {
+                urlList.forEach(function (element) {
+                    assert.ok(url.parse(element));
+                });
+                assert.ok(urlList.length > 1);
+                console.log('urlList size:' + urlList.length);
+            });
+        });
+    });
+
+    describe('#parseShow()', function () {
+
+        var urlWithFilm = 'http://tumejortorrent.com/descargar/peliculas-x264-mkv/coco-/bluray-microhd/';
+
+        it("should return the film 'coco' with all data", function () {
+            return tumejortorrent_scraper.parseShow(urlWithFilm).then(function (show) {
+                assert.equal(show.urlBase, 'http://tumejortorrent.com/descargar/peliculas-x264-mkv/coco-/bluray-microhd/');
+                assert.equal(show.title, 'Coco');
+                assert.ok(show.description);
+                assert.equal(show.quality, 'BluRay 720p X264 MKV');
+                assert.equal(show.fileSize, '2.5 GB');
+                assert.equal(show.urlwithCover, 'http://tumejortorrent.com/pictures/f/mediums/103770_-1518265235-coco--bluray-microhd.jpg');
+                assert.equal(show.releaseDate, '10-02-2018');
+                assert.equal(show.urltodownload, 'http://tumejortorrent.com/descargar-torrent/103770_-1518265235-coco--bluray-microhd/');
+                // console.log("Show:'" + JSON.stringify(show) + "'");
+            });
+        });
+    });
+
 });
