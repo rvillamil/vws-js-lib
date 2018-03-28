a9f0b55edb0a5ce9374a9b0fb44f7b1f872113e8
Rodrigo Villamil <rodrigo.villamil@diagroup.com>
2018-03-28 13:27:30 +0200

    new generic crawler API

--- examples/examples-tumejortorrent.js
+++ examples/examples-tumejortorrent.js
@@ -4,11 +4,11 @@ const tumejortorrent = require('../lib/tumejortorrent');
 // Example use
 //
 tumejortorrent.crawlBillboardFilms(
-    show => console.log('Show: ', showObjectCrawled))
-.then(
-    showList => {
-        console.log("billboardfilms length: " + showList.length);        
-    }
-).catch(function (err) {
-    console.log('Error: ' + err);
-});
+        show => console.log('Show: ', show))
+    .then(
+        showList => {
+            console.log("billboardfilms length: " + showList.length);
+        }
+    ).catch(function (err) {
+        console.log('Error: ' + err);
+    });
