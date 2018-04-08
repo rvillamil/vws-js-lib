b5dbe8874d6b1d1dc9e37616c9c28c545ff9bf08
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2018-04-08 17:19:09 +0200

    New TVShowlink object

--- test/test_show.js
+++ test/test_show.js
@@ -13,12 +13,13 @@ function newTestShow(title, currentSession, currentEpisode) {
     toShow.currentSession = currentSession
     toShow.currentEpisode = currentEpisode
     toShow.urltodownload = `http://urltodownload_${title}_${currentSession}_${currentEpisode}`
+
     return toShow
 }
 
 describe('show object', function () {
 
-    describe('#mergeEpisodes()', function () {
+    describe('#newTVShowLinks(showList)', function () {
 
         favoritesList = [];
 
@@ -32,10 +33,20 @@ describe('show object', function () {
         favoritesList.push(fromShow3);
 
         it('Should add the episode list from shows to show', function () {
+            // console.log(`toShow antes  --> ${JSON.stringify(toShow)}\n\n`)
+            toShow.newTVShowLinks(favoritesList)
 
-            console.log(`toShow antes  --> ${JSON.stringify(toShow)}\n\n`)
-            toShow.mergeTVShowLinks(favoritesList);
-            console.log(`toShow despues  --> ${JSON.stringify(toShow)}\n\n`)
+            assert.equal(toShow.tvShowLinks[0].session, "5");
+            assert.equal(toShow.tvShowLinks[0].episode, "2");
+            assert.equal(toShow.tvShowLinks[0].urltodownload, "http://urltodownload_TVShow_5_2");
+
+            assert.equal(toShow.tvShowLinks[1].session, "5");
+            assert.equal(toShow.tvShowLinks[1].episode, "3");
+            assert.equal(toShow.tvShowLinks[1].urltodownload, "http://urltodownload_TVShow_5_3");
+
+            assert.equal(toShow.tvShowLinks[2].session, "5");
+            assert.equal(toShow.tvShowLinks[2].episode, "4");
+            assert.equal(toShow.tvShowLinks[2].urltodownload, "http://urltodownload_TVShow_5_4");
         });
     });
 });
