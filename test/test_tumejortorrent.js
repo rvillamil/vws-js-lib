26a97db9f6b5bd67150a1b0879029a05d5020ec1
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2018-04-10 20:03:31 +0200

    Fix test

--- test/test_tumejortorrent.js
+++ test/test_tumejortorrent.js
@@ -10,38 +10,38 @@ const tumejortorrent = require('../lib/tumejortorrent');
 describe('tumejortorrent', function () {
     this.timeout(15000);
     describe('#crawlURLsWithVideoPremieres()', function () {
-        it('should return a video premiere list with 2 url', function () {
+        it('should return a video premiere url list with 2 url', function () {
             return tumejortorrent.crawlURLsWithVideoPremieres(2).then(
-                showList => assert.ok(showList.length == 2))
+                urls => assert.ok(urls.length == 2))
         });
     });
 
     describe('#crawlURLsWithBillboardFilms()', function () {
-        it('should return a billboard film list with 2 url', function () {
+        it('should return a billboard film url list with 2 url', function () {
             return tumejortorrent.crawlURLsWithBillboardFilms(2).then(
-                showList => assert.ok(showList.length == 2))
+                urls => assert.ok(urls.length == 2))
         });
     });
 
-    describe('#crawlURLsWithTVShows()', function () {
-        it('should return a tvshow list with 3 url', function () {
-            return tumejortorrent.crawlURLsWithTVShows(3).then(
-                showList => assert.ok(showList.length == 3))
+    describe('#crawlURLsWithLatestTVShows()', function () {
+        it('should return a tvshow url list with 3 url', function () {
+            return tumejortorrent.crawlURLsWithLatestTVShows(3).then(
+                urls => assert.ok(urls.length == 3))
         });
     });
 
-    describe('#crawlEpisodesURL()', function () {
-        it('should return a tvshow list with 3 url', function () {
-            return tumejortorrent.crawlEpisodesURL('erase-una-vez/1490', 3).then(
-                showList => assert.ok(showList.length == 3))
+    describe('#crawlURLsWithCollection()', function () {
+        it('should return a tvshow url list with 3 url', function () {
+            return tumejortorrent.crawlURLsWithCollection(3, 'erase-una-vez/1490').then(
+                urls => assert.ok(urls.length == 3))
         });
     });
 
-    describe('#crawlShow()', function () {
+    describe('#crawlDataShow()', function () {
         var urlWithFilm = 'http://tumejortorrent.com/descargar/peliculas-x264-mkv/coco-/bluray-microhd/';
 
         it("should return the film 'Coco' with all data", function () {
-            return tumejortorrent.crawlShow(urlWithFilm).then(show => {
+            return tumejortorrent.crawlDataShow(urlWithFilm).then(show => {
                 //console.log("Show crawled:'" + JSON.stringify(show) + "'");
                 assert.equal(show.urlBase, 'http://tumejortorrent.com/descargar/peliculas-x264-mkv/coco-/bluray-microhd/');
                 assert.equal(show.title, 'Coco');
@@ -58,7 +58,7 @@ describe('tumejortorrent', function () {
         var urlWithTVShow = 'http://tumejortorrent.com/descargar/serie-en-hd/erase-una-vez/temporada-7/capitulo-14/';
 
         it("should return the TVShow 'Erase una vez', 7x14 with all data", function () {
-            return tumejortorrent.crawlShow(urlWithTVShow).then(show => {
+            return tumejortorrent.crawlDataShow(urlWithTVShow).then(show => {
                 //console.log(`TVShow Crawled:  ${JSON.stringify(show)}\n\n`);
                 assert.equal(show.urlBase, 'http://tumejortorrent.com/descargar/serie-en-hd/erase-una-vez/temporada-7/capitulo-14/');
                 assert.equal(show.title, 'Erase una Vez/Erase Una Vez - Temporada 7');
@@ -74,5 +74,9 @@ describe('tumejortorrent', function () {
                 assert.equal(show.urltodownload, 'http://tumejortorrent.com/descargar-torrent/105678_-1523024727-erase-una-vez---temporada-7--hdtv-720p-ac3-5-1/');
             });
         });
+    });
+
+    describe('#crawlDataShowCollection()', function () {
+        console.error("TODO....crawlDataShowCollection")
     });
 });
