409ccc03986ee45a55b07e72f6ed01b534f36151
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2018-04-16 22:12:21 +0200

    Fixing test

--- test/test_favoriteRepository.js
+++ test/test_favoriteRepository.js
@@ -14,9 +14,9 @@ var FavoriteRepository = require('../lib/favoriteRepository');
 
 
 describe('favoriteRepository', function () {
+    var favoriteRepository = new FavoriteRepository('vws-db');
 
     describe('#findAll()', function () {
-        var favoriteRepository = new FavoriteRepository('vws-db');
 
         it('Should load two Collection', function () {
 
@@ -26,30 +26,39 @@ describe('favoriteRepository', function () {
             var showCollection2 = new ShowCollection()
             showCollection2.name = "hola/mola_2"
 
-            return favoriteRepository.save(showCollection1)
+            favoriteRepository.save(showCollection1)
                 .catch(err => {
                     console.error("ERROR!" + err)
                     assert.ok(1 == 0) // Forzamos el pete del test
                 })
 
-            return favoriteRepository.save(showCollection2)
+            favoriteRepository.save(showCollection2)
                 .catch(err => {
                     console.error("ERROR!" + err)
                     assert.ok(1 == 0) // Forzamos el pete del test
                 })
 
-            return favoriteRepository.findAll().then(
+            favoriteRepository.findAll().then(
                 showCollectionList => {
+                    console.log("showCollectionList: " + JSON.stringify(showCollectionList))
+
                     assert.equal(showCollectionList.length, 2)
+                }
+            )
+            return favoriteRepository.deleteAll().then(
+                numRemoved => {
+                    assert.equal(numRemoved, 2)
                 }
             )
         });
 
-
         it('Not Should save duplicated collection name', function () {
 
             var showCollection1 = new ShowCollection()
-            showCollection1.name = "hola/mola_2"
+            showCollection1.name = "hola/mola_1"
+
+            var showCollection2 = new ShowCollection()
+            showCollection2.name = "hola/mola_1"
 
             favoriteRepository.save(showCollection1)
                 .catch(err => {
@@ -57,11 +66,69 @@ describe('favoriteRepository', function () {
                     //console.error(err)
                 })
 
-            return favoriteRepository.findAll().then(
+            favoriteRepository.save(showCollection2)
+                .catch(err => {
+                    assert(err)
+                    //console.error(err)
+                })
+
+            favoriteRepository.findAll().then(
                 showCollectionList => {
-                    assert.equal(showCollectionList.length, 2)
+                    assert.equal(showCollectionList.length, 1)
+                }
+            )
+
+            return favoriteRepository.deleteAll().then(
+                numRemoved => {
+                    assert.equal(numRemoved, 2)
                 }
             )
         });
     });
+
+    describe('#delete()', function () {
+
+        it('Should persistir two Collection more and delete one', function () {
+
+            var showCollection1 = new ShowCollection()
+            showCollection1.name = "hola/mola_1"
+
+            var showCollection2 = new ShowCollection()
+            showCollection2.name = "hola/mola_2"
+
+            favoriteRepository.save(showCollection1)
+                .catch(err => {
+                    console.error("ERROR!" + err)
+                    assert.ok(1 == 0) // Forzamos el pete del test
+                })
+
+            favoriteRepository.save(showCollection2)
+                .catch(err => {
+                    console.error("ERROR!" + err)
+                    assert.ok(1 == 0) // Forzamos el pete del test
+                })
+
+            favoriteRepository.findAll().then(
+                showCollectionList => {
+                    console.log("showCollectionList: " + JSON.stringify(showCollectionList))
+                    assert.equal(showCollectionList.length, 2)
+                })
+
+            favoriteRepository.delete("hola/mola_2")
+                .catch(err => {
+                    console.error("ERROR!" + err)
+                    assert.ok(1 == 0) // Forzamos el pete del test
+                })
+
+            return favoriteRepository.findAll().then(
+                showCollectionList => {
+                    assert.equal(showCollectionList.length, 1)
+                }
+            ).catch(err => {
+                console.error("ERROR!" + err)
+                assert.ok(1 == 0) // Forzamos el pete del test
+            })
+        });
+    });
+
 });
