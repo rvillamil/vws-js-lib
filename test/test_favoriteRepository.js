273ed41ff8c01549ab323af5f4b5a90e5b1fd209
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2018-04-22 10:48:23 +0200

    New method for find collecion by name

--- test/test_favoriteRepository.js
+++ test/test_favoriteRepository.js
@@ -117,7 +117,7 @@ describe('favoriteRepository', function () {
                     assert.ok(1 == 0) // Forzamos el pete del test
                 })
 
-            return favoriteRepository.findAll().then(
+            favoriteRepository.findAll().then(
                 showCollectionList => {
                     assert.equal(showCollectionList.length, 1)
                 }
@@ -125,8 +125,83 @@ describe('favoriteRepository', function () {
                 console.error("ERROR!" + err)
                 assert.ok(1 == 0) // Forzamos el pete del test
             })
+
+            return favoriteRepository.deleteAll().then(
+                numRemoved => {
+                    assert.equal(numRemoved, 1)
+                }
+            )
+
         });
 
     });
 
+    describe('#findByCollectionName()', function () {
+
+        it('Should load one Collection by', function () {
+
+            var showCollection1 = new ShowCollection()
+            showCollection1.name = "hola/mola_1"
+
+            var showCollection2 = new ShowCollection()
+            showCollection2.name = "hola/mola_2"
+
+            favoriteRepository.save(showCollection1)
+                .catch(err => {
+                    console.error("ERROR!" + err)
+                    assert.ok(1 == 0) // Forzamos el pete del test
+                })
+
+            favoriteRepository.save(showCollection2)
+                .catch(err => {
+                    console.error("ERROR!" + err)
+                    assert.ok(1 == 0) // Forzamos el pete del test
+                })
+
+            favoriteRepository.findByCollectionName('hola/mola_1').then(
+                showCollectionList => {
+                    assert.equal(showCollectionList.length, 1)
+                }
+            )
+            return favoriteRepository.deleteAll().then(
+                numRemoved => {
+                    assert.equal(numRemoved, 2)
+                }
+            )
+        });
+
+
+        it('it should not load one Collection by collection name', function () {
+
+            var showCollection1 = new ShowCollection()
+            showCollection1.name = "hola/mola_1"
+
+            var showCollection2 = new ShowCollection()
+            showCollection2.name = "hola/mola_2"
+
+            favoriteRepository.save(showCollection1)
+                .catch(err => {
+                    console.error("ERROR!" + err)
+                    assert.ok(1 == 0) // Forzamos el pete del test
+                })
+
+            favoriteRepository.save(showCollection2)
+                .catch(err => {
+                    console.error("ERROR!" + err)
+                    assert.ok(1 == 0) // Forzamos el pete del test
+                })
+
+            favoriteRepository.findByCollectionName('hola/mola_3').then(
+                showCollectionList => {
+                    assert.equal(showCollectionList.length, 0)
+                }
+            )
+            return favoriteRepository.deleteAll().then(
+                numRemoved => {
+                    assert.equal(numRemoved, 2)
+                }
+            )
+        });
+
+    });
 });
