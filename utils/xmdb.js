300f5d4fd97906b38e3e349b8ea7427fe9b2d4b7
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2018-05-13 17:54:01 +0200

    New util to find data in movie DB webs

---
+++ utils/xmdb.js
@@ -0,0 +1,14 @@
+const omdb = require('../lib/omdb');
+const tmdb = require('../lib/tmdb');
+const crawler = require('../lib/crawler');
+const Show = require('../lib/show');
+
+var title = "Vengadores Infinity War"
+show = new Show()
+show.title = title
+
+crawler.searchShowInXMDB(show)
+    .then(show => {
+        console.log(`------------------------------------`)
+        console.log(`Show Crawler: ${JSON.stringify(show)}`)
+    })
