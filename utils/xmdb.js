81764a83e6e8822c7062d4e7520faaa4f388b4cb
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2018-05-15 12:56:23 +0200

    bug fixed

--- utils/xmdb.js
+++ utils/xmdb.js
@@ -3,12 +3,35 @@ const tmdb = require('../lib/tmdb');
 const crawler = require('../lib/crawler');
 const Show = require('../lib/show');
 
-var title = "Peelers"
-show = new Show()
-show.title = title
+function processParamTitle() {
+    if (process.argv.length > 3) {
+        console.log("ERROR! Argument list error")
+        help()
+        process.exit(1)
+    }
+    var titleToSearch = process.argv[2]
+    if (!titleToSearch) {
+        console.log("ERROR! Need the title to search")
+        help()
+        process.exit(1)
+    }
+    return titleToSearch
+}
 
-return crawler.searchShowInXMDB(show)
-    .then(show => {
-        console.log(`------------------------------------`)
-        console.log(`Show Crawler: ${JSON.stringify(show)}`)
-    })
+function help() {
+    console.log(`Search a film in TMDB and OMDB databases`)
+    console.log(`   Usage: ${process.argv[1]} 'film title'`)
+}
+
+function searchFilmByTitle(title) {
+    show = new Show()
+    show.title = title
+
+    return crawler.searchShowInXMDB(show, kind = 'movie', debug = true)
+        .then(show => {
+            console.log(`------------------------------------`)
+            console.log(`Show: ${JSON.stringify(show)}`)
+        })
+}
+
+searchFilmByTitle(processParamTitle())
