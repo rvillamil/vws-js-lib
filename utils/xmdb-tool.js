dd903594ff251537c1109bcbb41ac50ba5ac14af
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2018-10-12 12:57:34 +0200

    New favorites tool management console

---
+++ utils/xmdb-tool.js
@@ -0,0 +1,37 @@
+const omdb = require('../lib/omdb');
+const tmdb = require('../lib/tmdb');
+const crawler = require('../lib/crawler');
+const Show = require('../lib/show');
+
+function processParamTitle() {
+    if (process.argv.length > 3) {
+        console.log("ERROR! Argument list error")
+        help()
+        process.exit(1)
+    }
+    var titleToSearch = process.argv[2]
+    if (!titleToSearch) {
+        console.log("ERROR! Need the title to search")
+        help()
+        process.exit(1)
+    }
+    return titleToSearch
+}
+
+function help() {
+    console.log(`Search a film in TMDB and OMDB databases`)
+    console.log(`   Usage: ${process.argv[1]} 'film title'`)
+}
+
+function searchFilmByTitle(title) {
+    show = new Show()
+    show.title = title
+
+    return crawler.searchShowInXMDB(show, kind = 'movie', debug = true)
+        .then(show => {
+            console.log(`------------------------------------`)
+            console.log(`Show: ${JSON.stringify(show)}`)
+        })
+}
+
+searchFilmByTitle(processParamTitle())
