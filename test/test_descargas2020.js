e1f3f1a8e32a864a3d4fb4c8edbcd783a2178978
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-07-05 18:42:38 +0200

    New problem to fix..

--- test/test_descargas2020.js
+++ test/test_descargas2020.js
@@ -99,5 +99,35 @@ describe("descargas2020", function() {
                 );
             });
         });
+
+        it("should return the TVShow 'La resistencia' 2x157 (aunque la URL ponga temporada 1)", function() {
+            var urlWithTVShow =
+                "https://descargas2020.org/descargar/serie-en-hd/la-resistencia/temporada-1/capitulo-56-al-57/";
+
+            return descargas2020.crawlDataShow(urlWithTVShow).then(show => {
+                //console.log(`TVShow Crawled:  ${JSON.stringify(show)}\n\n`);
+                assert.equal(
+                    show.urlBase,
+                    "https://descargas2020.org/descargar/serie-en-hd/la-resistencia/temporada-1/capitulo-56-al-57/"
+                );
+                assert.equal(show.title, "La Resistencia");
+                assert.equal(show.year, "2019");
+                assert.equal(show.currentSession, "2");
+                assert.equal(show.currentEpisode, "157");
+                assert.ok(show.description);
+                assert.ok(show.sinopsis);
+                assert.equal(show.quality, "HDTV 720p");
+                assert.equal(show.fileSize, "3.9 GB");
+                assert.equal(
+                    show.urlwithCover,
+                    "https://descargas2020.org/pictures/c/thumbs/1618_elementary.jpg"
+                );
+                assert.equal(show.releaseDate, "02-07-2019");
+                assert.equal(
+                    show.urltodownload,
+                    "https:////descargas2020.org/descargar-torrent/124369_-1562035403-Elementary---Temporada-7--HDTV-720p-AC3-5-1"
+                );
+            });
+        });
     });
 });
