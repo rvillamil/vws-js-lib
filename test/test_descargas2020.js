6f5541d5251ab8aaf7950ce1bf789e2e99d143d7
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-07-05 18:27:52 +0200

    Test fixed

--- test/test_descargas2020.js
+++ test/test_descargas2020.js
@@ -4,85 +4,99 @@
 // - mocha ...: https://mochajs.org/
 // - assert ..: https://nodejs.org/api/all.html
 //
-const assert = require('assert');
-const descargas2020 = require('../lib/crawlers/descargas2020');
+const assert = require("assert");
+const descargas2020 = require("../lib/crawlers/descargas2020");
 
-describe('descargas2020', function() {
+describe("descargas2020", function() {
     this.timeout(15000);
-    describe('#crawlLinkToURLsWithVideoPremieres()', function() {
-        it('should return a video premiere url list with 2 url', function() {
-            return descargas2020.crawlLinkToURLsWithVideoPremieres(2).then(
-                urls => assert.ok(urls.length == 2))
+    describe("#crawlLinkToURLsWithVideoPremieres()", function() {
+        it("should return a video premiere url list with 2 url", function() {
+            return descargas2020.crawlLinkToURLsWithVideoPremieres(2).then(urls => {
+                assert.ok(urls.length == 2);
+            });
         });
     });
 
-    describe('#crawlLinkToURLsWithBillboardFilms()', function() {
-        it('should return a billboard film url list with 2 url', function() {
-            return descargas2020.crawlLinkToURLsWithBillboardFilms(2).then(
-                urls => assert.ok(urls.length == 2))
+    describe("#crawlLinkToURLsWithBillboardFilms()", function() {
+        it("should return a billboard film url list with 2 url", function() {
+            return descargas2020
+                .crawlLinkToURLsWithBillboardFilms(2)
+                .then(urls => assert.ok(urls.length == 2));
         });
     });
 
-    describe('#crawlLinkToURLsWithLatestTVShows()', function() {
-        it('should return a tvshow url list with 3 url', function() {
-            return descargas2020.crawlLinkToURLsWithLatestTVShows(3).then(
-                urls => assert.ok(urls.length == 3))
+    describe("#crawlLinkToURLsWithLatestTVShows()", function() {
+        it("should return a tvshow url list with 3 url", function() {
+            return descargas2020
+                .crawlLinkToURLsWithLatestTVShows(3)
+                .then(urls => assert.ok(urls.length == 3));
         });
     });
 
-    describe('#crawlLinkToURLsWithEpisodes()', function() {
-        it('should return a tvshow url list with 3 url', function() {
-            return descargas2020.crawlLinkToURLsWithEpisodes(3, 'erase-una-vez/1490').then(
-                urls => assert.ok(urls.length == 3))
+    describe("#crawlLinkToURLsWithEpisodes()", function() {
+        it("should return a tvshow url list with 3 url", function() {
+            return descargas2020
+                .crawlLinkToURLsWithEpisodes(3, "erase-una-vez/1490")
+                .then(urls => assert.ok(urls.length == 3));
         });
     });
 
-    describe('#crawlDataShow()', function() {
-        var urlWithFilm = 'https://tumejortorrent.com/descargar/peliculas-x264-mkv/coco-/bluray-microhd/';
+    describe("#crawlDataShowFromPeliculasMKV()", function() {
+        var urlWithFilm =
+            "https://descargas2020.org/descargar/peliculas-x264-mkv/300/";
 
-        it("should return the film 'Coco' with all data", function() {
+        it("should return the film '300' with all data", function() {
             return descargas2020.crawlDataShow(urlWithFilm).then(show => {
                 //console.log("Show crawled:'" + JSON.stringify(show) + "'");
-                assert.equal(show.urlBase, 'https://tumejortorrent.com/descargar/peliculas-x264-mkv/coco-/bluray-microhd/');
-                assert.equal(show.title, 'Coco');
+                assert.equal(
+                    show.urlBase,
+                    "https://descargas2020.org/descargar/peliculas-x264-mkv/300/"
+                );
+                assert.equal(show.title, "300");
                 assert.ok(show.description);
-                assert.equal(show.quality, 'BluRay 720p X264 MKV');
-                assert.equal(show.fileSize, '2.5 GB');
-                assert.equal(show.urlwithCover, 'https://tumejortorrent.com/pictures/f/mediums/103770_-1518265235-coco--bluray-microhd.jpg');
-                assert.equal(show.releaseDate, '10-02-2018');
-                assert.equal(show.urltodownload, 'https://tumejortorrent.com/descargar-torrent/103770_-1518265235-coco--bluray-microhd/');
-                assert.equal(show.originalTitle, 'Coco');
+                assert.equal(show.quality, "BluRay 720p X264 MKV");
+                assert.equal(show.fileSize, "2.7 GB");
+                assert.equal(
+                    show.urlwithCover,
+                    "https://descargas2020.org/pictures/f/mediums/124384_-1562062800-300--BluRay-MicroHD.jpg"
+                );
+                assert.equal(show.releaseDate, "02-07-2019");
+                assert.equal(
+                    show.urltodownload,
+                    "https:////descargas2020.org/descargar-torrent/124384_-1562062800-300--BluRay-MicroHD"
+                );
             });
         });
+    });
 
-        var urlWithFilm2 = 'https://tumejortorrent.com/descargar/peliculas-castellano/estrenos-de-cine/ready-player-one-/ts-screener/'
+    describe("#crawlDataShowFromURLWithTVShow()", function() {
+        var urlWithTVShow =
+            "https://descargas2020.org/descargar/serie-en-hd/elementary/temporada-7/capitulo-05/";
 
-        it("should return the film 'Ready Player oner' with all data", function() {
-            return descargas2020.crawlDataShow(urlWithFilm2).then(show => {
-                //console.log("Show crawled:'" + JSON.stringify(show) + "'");
-                assert.equal(show.title, 'Ready Player One');
-                assert.equal(show.originalTitle, 'Ready Player One');
-            });
-        });
-
-
-        var urlWithTVShow = 'https://tumejortorrent.com/descargar/serie-en-hd/erase-una-vez/temporada-7/capitulo-14/';
-
-        it("should return the TVShow 'Erase una vez', 7x14 with all data", function() {
+        it("should return the TVShow 'Elementary' 7x05", function() {
             return descargas2020.crawlDataShow(urlWithTVShow).then(show => {
                 //console.log(`TVShow Crawled:  ${JSON.stringify(show)}\n\n`);
-                assert.equal(show.urlBase, 'https://tumejortorrent.com/descargar/serie-en-hd/erase-una-vez/temporada-7/capitulo-14/');
-                assert.equal(show.title, 'Erase Una Vez');
-                assert.equal(show.year, '2018');
-                assert.equal(show.currentSession, '7');
-                assert.equal(show.currentEpisode, '14');
+                assert.equal(
+                    show.urlBase,
+                    "https://descargas2020.org/descargar/serie-en-hd/elementary/temporada-7/capitulo-05/"
+                );
+                assert.equal(show.title, "Elementary");
+                assert.equal(show.year, "2019");
+                assert.equal(show.currentSession, "7");
+                assert.equal(show.currentEpisode, "05");
                 assert.ok(show.description);
                 assert.ok(show.sinopsis);
-                assert.equal(show.quality, 'HDTV 720p');
-                assert.equal(show.fileSize, '1.6 GB');
-                assert.equal(show.urlwithCover, 'https://tumejortorrent.com/pictures/c/1490_erase-una-vez.jpg');
-                assert.equal(show.releaseDate, '06-04-2018');
-                assert.equal(show.urltodownload, 'https://tumejortorrent.com/descargar-torrent/105678_-1523024727-erase-una-vez---temporada-7--hdtv-720p-ac3-5-1/');
+                assert.equal(show.quality, "HDTV 720p");
+                assert.equal(show.fileSize, "1.5 GB");
+                assert.equal(
+                    show.urlwithCover,
+                    "https://descargas2020.org/pictures/c/thumbs/1618_elementary.jpg"
+                );
+                assert.equal(show.releaseDate, "02-07-2019");
+                assert.equal(
+                    show.urltodownload,
+                    "https:////descargas2020.org/descargar-torrent/124369_-1562035403-Elementary---Temporada-7--HDTV-720p-AC3-5-1"
+                );
             });
         });
     });
