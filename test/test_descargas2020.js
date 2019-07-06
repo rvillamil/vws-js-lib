1a3c6d3bff51cf84282749299c9b206606f86388
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-07-06 14:59:43 +0200

    Test bug fixed

--- test/test_descargas2020.js
+++ test/test_descargas2020.js
@@ -63,7 +63,7 @@ describe("descargas2020", function() {
                 assert.equal(show.releaseDate, "02-07-2019");
                 assert.equal(
                     show.urltodownload,
-                    "https:////descargas2020.org/descargar-torrent/124384_-1562062800-300--BluRay-MicroHD"
+                    "https://descargas2020.org/descargar-torrent/124384_-1562062800-300--BluRay-MicroHD"
                 );
             });
         });
@@ -95,12 +95,12 @@ describe("descargas2020", function() {
                 assert.equal(show.releaseDate, "02-07-2019");
                 assert.equal(
                     show.urltodownload,
-                    "https:////descargas2020.org/descargar-torrent/124369_-1562035403-Elementary---Temporada-7--HDTV-720p-AC3-5-1"
+                    "https://descargas2020.org/descargar-torrent/124369_-1562035403-Elementary---Temporada-7--HDTV-720p-AC3-5-1"
                 );
             });
         });
 
-        it("should return the TVShow 'La resistencia' 2x157 (aunque la URL ponga temporada 1)", function() {
+        it("should return the TVShow 'La resistencia' 2x156 (aunque la URL ponga temporada 1)", function() {
             var urlWithTVShow =
                 "https://descargas2020.org/descargar/serie-en-hd/la-resistencia/temporada-1/capitulo-56-al-57/";
 
@@ -113,19 +113,18 @@ describe("descargas2020", function() {
                 assert.equal(show.title, "La Resistencia");
                 assert.equal(show.year, "2019");
                 assert.equal(show.currentSession, "2");
-                assert.equal(show.currentEpisode, "157");
+                assert.equal(show.currentEpisode, "156");
                 assert.ok(show.description);
-                assert.ok(show.sinopsis);
                 assert.equal(show.quality, "HDTV 720p");
                 assert.equal(show.fileSize, "3.9 GB");
                 assert.equal(
                     show.urlwithCover,
-                    "https://descargas2020.org/pictures/c/thumbs/1618_elementary.jpg"
+                    "https://descargas2020.org/pictures/c/thumbs/4116_la-resistencia.jpg"
                 );
-                assert.equal(show.releaseDate, "02-07-2019");
+                assert.equal(show.releaseDate, "05-07-2019");
                 assert.equal(
                     show.urltodownload,
-                    "https:////descargas2020.org/descargar-torrent/124369_-1562035403-Elementary---Temporada-7--HDTV-720p-AC3-5-1"
+                    "https://descargas2020.org/descargar-torrent/124523_-1562334660-La-Resistencia---Temporada-2--HDTV-720p-AC3-5-1"
                 );
             });
         });
