9d0bcb5c9ecce80005882c52086ccb9c5861ae46
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-07-07 14:58:09 +0200

    Fix eslint problems

--- utils/dbutils.js
+++ utils/dbutils.js
@@ -1,10 +1,9 @@
-var ShowCollection = require('../lib/model/showCollection');
-var FavoriteRepository = require('../lib/repositories/favoriteRepository');
-const path = require('path');
+var ShowCollection = require('../lib/model/showCollection')
+var FavoriteRepository = require('../lib/repositories/favoriteRepository')
 
 function processArgsNumber() {
     if (process.argv.length > 5) {
-        console.log("ERROR! Number of arguments error. 2 parameters are requiered")
+        console.log('ERROR! Number of arguments error. 2 parameters are requiered')
         help()
         process.exit(1)
     }
@@ -14,7 +13,7 @@ function processArgsNumber() {
 function processOptionArgument() {
     var option = process.argv[2]
     if (!option) {
-        console.log("ERROR! wrong parameters")
+        console.log('ERROR! wrong parameters')
         help()
         process.exit(1)
     }
@@ -22,7 +21,7 @@ function processOptionArgument() {
         (option != '-s') &&
         (option != '-l')) {
 
-        console.log("ERROR! wrong parameters")
+        console.log('ERROR! wrong parameters')
         help()
         process.exit(1)
     }
@@ -33,7 +32,7 @@ function processOptionArgument() {
 function processUserDataPath() {
     var userDataPath = process.argv[3]
     if (!userDataPath) {
-        console.log("ERROR! wrong parameters")
+        console.log('ERROR! wrong parameters')
         help()
         process.exit(1)
     }
@@ -47,12 +46,12 @@ function processParamTitle() {
     var option = process.argv[2]
 
     if (!titleToSearch) {
-        console.log("ERROR! wrong parameters")
+        console.log('ERROR! wrong parameters')
         help()
         process.exit(1)
     }
     if ((!titleToSearch) && (option == '-l')) {
-        console.log("ERROR! wrong parameters")
+        console.log('ERROR! wrong parameters')
         help()
         process.exit(1)
     }
@@ -61,11 +60,11 @@ function processParamTitle() {
 
 function help() {
     console.log(`Usage: ${process.argv[1]} [-s|-d|-l] 'database path' [show collection name]`)
-    console.log(`-s: save show collection`)
-    console.log(`-d: delete show collection`)
-    console.log(`-l: list show collection`)
-    console.log(`     e.g.: ./dbutils -l '/Users/Name/Library/Application Support/Video Website Scraper/vws-db'`)
-    console.log(`     e.g.: ./dbutils -s '/Users/Name/Library/Application Support/Video Website Scraper/vws-db' 'pp/ll'`)
+    console.log('-s: save show collection')
+    console.log('-d: delete show collection')
+    console.log('-l: list show collection')
+    console.log('     e.g.: ./dbutils -l \'/Users/Name/Library/Application Support/Video Website Scraper/vws-db\'')
+    console.log('     e.g.: ./dbutils -s \'/Users/Name/Library/Application Support/Video Website Scraper/vws-db\' \'pp/ll\'')
 }
 
 function saveFavoriteShowCollection(showCollectionName) {
@@ -100,7 +99,7 @@ function deleteFavoriteShowCollection(showCollectionName) {
 function listAllFavoritesShowCollection() {
     return favoriteRepository.findAll().then(
         showCollectionList => {
-            showCollectionList.forEach(function(showCollection) {
+            showCollectionList.forEach(function (showCollection) {
                 console.log(`Show collection saved!: ${JSON.stringify(showCollection)}\n`)
             })
         }
@@ -112,8 +111,8 @@ function listAllFavoritesShowCollection() {
 // Global var
 //
 processArgsNumber()
-option = processOptionArgument()
-var favoriteRepository = new FavoriteRepository(processUserDataPath());
+var option = processOptionArgument()
+var favoriteRepository = new FavoriteRepository(processUserDataPath())
 if (option == '-d') {
     deleteFavoriteShowCollection(processParamTitle())
 } else if (option == '-s') {
--- utils/xmdb-tool.js
+++ utils/xmdb-tool.js
@@ -1,17 +1,15 @@
-const omdb = require('../lib/omdb');
-const tmdb = require('../lib/tmdb');
-const crawler = require('../lib/crawler');
-const Show = require('../lib/model/show');
+const crawler = require('../lib/crawler')
+const Show = require('../lib/model/show')
 
 function processParamTitle() {
     if (process.argv.length > 3) {
-        console.log("ERROR! Argument list error")
+        console.log('ERROR! Argument list error')
         help()
         process.exit(1)
     }
     var titleToSearch = process.argv[2]
     if (!titleToSearch) {
-        console.log("ERROR! Need the title to search")
+        console.log('ERROR! Need the title to search')
         help()
         process.exit(1)
     }
@@ -19,17 +17,17 @@ function processParamTitle() {
 }
 
 function help() {
-    console.log(`Search a film in TMDB and OMDB databases`)
+    console.log('Search a film in TMDB and OMDB databases')
     console.log(`   Usage: ${process.argv[1]} 'film title'`)
 }
 
 function searchFilmByTitle(title) {
-    show = new Show()
+    var show = new Show()
     show.title = title
 
-    return crawler.searchShowInXMDB(show, kind = 'movie', debug = true)
+    return crawler.searchShowInXMDB(show, 'movie', true)
         .then(show => {
-            console.log(`------------------------------------`)
+            console.log('------------------------------------')
             console.log(`Show: ${JSON.stringify(show)}`)
         })
 }
