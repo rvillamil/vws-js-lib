9908032dfdc703b2d6af89e423a8a6990c162312
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-11-22 17:27:54 +0100

    Dontorrent

--- test/test_dontorrent.js
+++ test/test_dontorrent.js
@@ -11,11 +11,22 @@ const dontorrent = require('../lib/crawlers/dontorrent')
 
 describe('dontorrent', function () {
     this.timeout(15000)
+
     describe('#crawlLinkToURLsWithVideoPremieres()', function () {
         it('should return a video premiere url list with 5 url', function () {
             return dontorrent.crawlLinkToURLsWithVideoPremieres(5).then(urls => {
                 assert.ok(urls.length == 5)
+                for (var i = 0; i < urls.length; i++) {
+                    assert.ok(urls[i].includes('pelicula/'))
+                }
             })
+        })
+    })
+
+
+    describe('#crawlLinkToURLsWithBillboardFilms()', function () {
+        it('should return a billboard film url list with 0 url', function () {
+            return dontorrent.crawlLinkToURLsWithBillboardFilms(2).then(urls => assert.ok(urls.length == 0))
         })
     })
 })
