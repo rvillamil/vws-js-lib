0233da54991f74516cc86991826cb1c24d96ef6f
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-11-24 19:18:13 +0100

    New test

--- test/test_dontorrent.js
+++ test/test_dontorrent.js
@@ -37,11 +37,9 @@ describe('dontorrent', function () {
         it('should return the film \'Vengadores: Endgame\' with all data', function () {
             return dontorrent.crawlDataShow(urlWithFilm).then(show => {
                 //console.log("Show crawled:'" + JSON.stringify(show) + "'");
-                assert.equal(
-                    show.urlBase,
-                    'https://dontorrent.org/pelicula/21442/Vengadores-Endgame-Open-Matte-Imax'
-                )
-                assert.equal(show.title, 'Vengadores: Endgame')
+                assert.equal(show.urlBase, 'https://dontorrent.org/pelicula/21442/Vengadores-Endgame-Open-Matte-Imax')
+                assert.equal(show.title, 'Vengadores: Endgame (Open Matte Imax)')
+                assert.ok(show.sinopsis)
                 assert.ok(show.description)
                 assert.equal(show.quality, 'HDRip')
                 assert.equal(show.fileSize, '2,85 GB')
@@ -49,12 +47,13 @@ describe('dontorrent', function () {
                     show.urlwithCover,
                     'https://blazing.network/imagenes/peliculas/Vengadores%20Endgame.jpg'
                 )
-                assert.equal(show.releaseDate, '22-11-2019')
                 assert.equal(show.year, '2019')
                 assert.equal(
                     show.urltodownload,
                     'https://blazing.network/torrents/peliculas/Vengadores_Endgame_OPEN_MATTE.torrent'
                 )
+                assert.equal(show.releaseDate, '') // dontorrent no proporciona este dato
+                assert.equal(show.originalTitle, '') // dontorrent no proporciona este dato
             })
         })
     })
