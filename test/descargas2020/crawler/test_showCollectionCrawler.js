3bde21b8bbf262e02813b1f4067de5fd9a6559b1
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-12-01 09:12:03 +0100

    More test

--- test/descargas2020/crawler/test_showCollectionCrawler.js
+++ test/descargas2020/crawler/test_showCollectionCrawler.js
@@ -10,31 +10,33 @@ const assert = require('assert')
 const showCollectionCrawler = require('../../../lib/crawlers/descargas2020/crawler/tvshowCollectionCrawler')
 
 describe('descargas2020/crawler/showCollectionCrawler', function () {
-    it('should return five episodes from tvshow collection \'Watchmen\'', function () {
+    describe('crawlDataShowCollection', function () {
+        it('should return 5 episodes from tvshow collection \'Watchmen\'', function () {
 
-        var uri = 'https://descargas2020.org/series-hd/watchmen/5258'
+            var uri = 'https://descargas2020.org/series-hd/watchmen/5258'
 
-        return showCollectionCrawler.crawlDataShowCollection(uri, 5)
-            .then(showCollection => {
-                assert.equal(showCollection.name, 'watchmen/5258')
-                assert.equal(showCollection.url, 'https://descargas2020.org/series-hd/watchmen/5258')
-                assert.ok(showCollection.shows)
-                assert.equal(showCollection.shows.length, 5)
-                showCollection.shows.forEach(show => {
-                    assert.ok(show.urlBase)
-                    assert.equal(show.title, 'Watchmen')
-                    assert.ok(show.sinopsis)
-                    assert.ok(show.description)
-                    assert.ok(show.quality)
-                    assert.ok(show.fileSize)
-                    assert.ok(show.urlwithCover)
-                    assert.ok(show.year)
-                    assert.ok(show.releaseDate)
-                    assert.ok(show.urltodownload)
-                    assert.equal(show.collectionName, showCollection.name)
-                    //assert.ok(show.originalTitle)
-                    //console.log(`TVShow Crawled:  ${JSON.stringify(show)}\n\n`);
+            return showCollectionCrawler.crawlDataShowCollection(uri, 5)
+                .then(showCollection => {
+                    assert.equal(showCollection.name, 'watchmen/5258')
+                    assert.equal(showCollection.url, 'https://descargas2020.org/series-hd/watchmen/5258')
+                    assert.ok(showCollection.shows)
+                    assert.equal(showCollection.shows.length, 5)
+                    showCollection.shows.forEach(show => {
+                        assert.ok(show.urlBase)
+                        assert.equal(show.title, 'Watchmen')
+                        assert.ok(show.sinopsis)
+                        assert.ok(show.description)
+                        assert.ok(show.quality)
+                        assert.ok(show.fileSize)
+                        assert.ok(show.urlwithCover)
+                        assert.ok(show.year)
+                        assert.ok(show.releaseDate)
+                        assert.ok(show.urltodownload)
+                        assert.equal(show.collectionName, showCollection.name)
+                        //assert.ok(show.originalTitle)
+                        //console.log(`TVShow Crawled:  ${JSON.stringify(show)}\n\n`);
+                    })
                 })
-            })
+        })
     })
 })
