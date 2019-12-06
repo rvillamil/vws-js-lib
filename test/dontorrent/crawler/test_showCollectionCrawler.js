4baa5712a4d2313fe829d8863c7a93f1ac03ac9f
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-12-06 19:14:47 +0100

    Test fixed

--- test/dontorrent/crawler/test_showCollectionCrawler.js
+++ test/dontorrent/crawler/test_showCollectionCrawler.js
@@ -11,7 +11,7 @@ const showCollectionCrawler = require('../../../lib/crawlers/dontorrent/crawler/
 
 describe('dontorrent/crawler/showCollectionCrawler', function () {
     describe('#crawlDataShowCollection()', function () {
-        it('should return the TVShow \'Mr Robot\' with all data', function () {
+        it('should return the TVShow \'Mr Robot\' with all episodes', function () {
 
             return showCollectionCrawler.crawlDataShowCollection('https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada').then(showCollection => {
                 //console.log("ShowCollection crawled:'" + JSON.stringify(showCollection) + "'");
@@ -32,6 +32,7 @@ describe('dontorrent/crawler/showCollectionCrawler', function () {
                         'https://blazing.network/imagenes/series/MrRobot4.jpg'
                     )
                     assert.equal(show.year, '2019')
+                    assert.equal(showCollection.shows.length, 7)
                     assert.ok(show.releaseDate)
                     assert.ok(show.urltodownload)
                     // assert.equal(show.originalTitle, '') // dontorrent no proporciona este dato
@@ -39,5 +40,64 @@ describe('dontorrent/crawler/showCollectionCrawler', function () {
                 })
             })
         })
+
+        it('should return the TVShow \'Mr Robot\' with 3 episodes max', function () {
+
+            return showCollectionCrawler.crawlDataShowCollection('https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada', 3).then(showCollection => {
+                //console.log("ShowCollection crawled:'" + JSON.stringify(showCollection) + "'");
+
+                assert.equal(showCollection.name, '63880/63881/Mr-Robot-4-Temporada')
+                assert.equal(showCollection.url, 'https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada')
+                assert.ok(showCollection.shows)
+                assert.ok(showCollection.shows.length > 0)
+                showCollection.shows.forEach(show => {
+                    assert.equal(show.urlBase, 'https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada')
+                    assert.equal(show.title, 'Mr. Robot')
+                    assert.ok(show.sinopsis)
+                    assert.ok(show.description)
+                    assert.equal(show.quality, 'HDTV')
+                    assert.equal(show.fileSize, 'N/A')
+                    assert.equal(
+                        show.urlwithCover,
+                        'https://blazing.network/imagenes/series/MrRobot4.jpg'
+                    )
+                    assert.equal(showCollection.shows.length, 3)
+                    assert.equal(show.year, '2019')
+                    assert.ok(show.releaseDate)
+                    assert.ok(show.urltodownload)
+                    // assert.equal(show.originalTitle, '') // dontorrent no proporciona este dato
+                })
+            })
+        })
+
+        it('should return the TVShow \'Mr Robot\' with 2 episodes max', function () {
+
+            return showCollectionCrawler.crawlDataShowCollection('https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada', 30).then(showCollection => {
+                //console.log("ShowCollection crawled:'" + JSON.stringify(showCollection) + "'");
+
+                assert.equal(showCollection.name, '63880/63881/Mr-Robot-4-Temporada')
+                assert.equal(showCollection.url, 'https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada')
+                assert.ok(showCollection.shows)
+                assert.ok(showCollection.shows.length > 0)
+                showCollection.shows.forEach(show => {
+                    assert.equal(show.urlBase, 'https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada')
+                    assert.equal(show.title, 'Mr. Robot')
+                    assert.ok(show.sinopsis)
+                    assert.ok(show.description)
+                    assert.equal(show.quality, 'HDTV')
+                    assert.equal(show.fileSize, 'N/A')
+                    assert.equal(
+                        show.urlwithCover,
+                        'https://blazing.network/imagenes/series/MrRobot4.jpg'
+                    )
+                    assert.equal(showCollection.shows.length, 7)
+                    assert.equal(show.year, '2019')
+                    assert.ok(show.releaseDate)
+                    assert.ok(show.urltodownload)
+                    // assert.equal(show.originalTitle, '') // dontorrent no proporciona este dato
+                })
+            })
+        })
+
     })
 })
