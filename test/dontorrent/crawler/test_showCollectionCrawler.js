a0b3c69c2aa084d6d1b653b0f4294ee113f8f3b2
Rodrigo Villamil Pérez <rodrigo.villamil@gmail.com>
2019-12-07 10:40:48 +0100

    Search TV shows on dontorrent

--- test/dontorrent/crawler/test_showCollectionCrawler.js
+++ test/dontorrent/crawler/test_showCollectionCrawler.js
@@ -99,5 +99,34 @@ describe('dontorrent/crawler/showCollectionCrawler', function () {
             })
         })
 
+        it('should return the TVShow \'Mr Robot\' with 1 episode', function () {
+
+            return showCollectionCrawler.crawlDataShowCollection('https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada', 1).then(showCollection => {
+                //console.log("ShowCollection crawled:'" + JSON.stringify(showCollection) + "'");
+
+                assert.equal(showCollection.name, '63880/63881/Mr-Robot-4-Temporada')
+                assert.equal(showCollection.url, 'https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada')
+                assert.ok(showCollection.shows)
+                assert.ok(showCollection.shows.length == 1)
+                showCollection.shows.forEach(show => {
+                    assert.equal(show.urlBase, 'https://dontorrent.org/serie/63880/63881/Mr-Robot-4-Temporada')
+                    assert.equal(show.title, 'Mr. Robot')
+                    assert.ok(show.sinopsis)
+                    assert.ok(show.description)
+                    assert.equal(show.quality, 'HDTV')
+                    assert.equal(show.fileSize, 'N/A')
+                    assert.equal(
+                        show.urlwithCover,
+                        'https://blazing.network/imagenes/series/MrRobot4.jpg'
+                    )
+                    assert.equal(showCollection.shows.length, 1)
+                    assert.equal(show.year, '2019')
+                    assert.ok(show.releaseDate)
+                    assert.ok(show.urltodownload)
+                    // assert.equal(show.originalTitle, '') // dontorrent no proporciona este dato
+                })
+            })
+        })
+
     })
 })
