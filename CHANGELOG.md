3c2411b64763f0bd787650aa061d2376219311c4
Rodrigo <rodrigo.villamil@gmail.com>
2021-02-25 16:38:39 +0100

    New url to download and minor bug fixeds

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [4.0.3] - 2020-02-25
+
+- Minor bug fixed
+  
 ## [4.0.2] - 2020-12-02
 
 - pctmix.com site replace descargas2020 site
--- test/pctmix/crawler/test_filmCrawler.js
+++ test/pctmix/crawler/test_filmCrawler.js
@@ -34,8 +34,37 @@ describe('pctmix/crawler/filmCrawler', function () {
                 assert.equal(show.year, '2020')
                 assert.equal(
                     show.urltodownload,
-                    'https://pctmix.com/download/143973_-1606890987-Ejecucion-inminente--1999---BluRay-MicroHD.torrent'
+                    'https://pctmix.com/download/143973_-1606890987-Ejecucion-inminente--1999---BluRay-MicroHD.torrent'                    
+                )
 
+                //assert.equal(show.originalTitle, '')
+            })
+        })
+
+        it('should return the film \'Richard dice adios \' with all data', function () {
+            //https://pctmix.com/descargar/peliculas-x264-mkv/richard-dice-adios-2021-/bluray-microhd/
+            var urlWithFilm=`https://${siteConstants.DOMAIN}/descargar/peliculas-x264-mkv/richard-dice-adios-2021-/bluray-microhd/`
+                            
+            
+            return filmCrawler.crawlDataFilm(urlWithFilm).then(show => {
+                //console.log("Show crawled:'" + JSON.stringify(show) + "'");
+                assert.equal(show.urlBase, urlWithFilm)
+                assert.equal(show.title, 'Richard dice adios')
+                //assert.ok (show.originalTitle)
+                assert.ok(show.sinopsis)
+                assert.ok(show.description)
+                assert.equal(show.quality, 'BluRay 720p X264 MKV')
+                assert.equal(show.fileSize, '2.7 GB')
+                assert.equal(show.releaseDate, '25-02-2021')
+                assert.equal(
+                    show.urlwithCover,
+                    'https://pctmix.com/pictures/f/mediums/147287_-1614253519-Richard-dice-adios--2021---BluRay-MicroHD.jpg'
+                )
+                assert.equal(show.year, '2021')
+                assert.equal(
+                    show.urltodownload,
+                    //'https://pctmix.com/download/143973_-1606890987-Ejecucion-inminente--1999---BluRay-MicroHD.torrent'                    
+                    'https://pctmix.com/descargar-torrent/147287_-1614253519-Richard-dice-adios--2021---BluRay-MicroHD/'
                 )
 
                 //assert.equal(show.originalTitle, '')
