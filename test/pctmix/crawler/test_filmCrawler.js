2d1bc7b0164941ccaacae13f0006c4e0946af993
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2021-07-11 20:06:43 +0200

    Agent OMDB migrated

--- test/pctmix/crawler/test_filmCrawler.js
+++ test/pctmix/crawler/test_filmCrawler.js
@@ -12,36 +12,36 @@ const siteConstants = require('../../../lib/crawlers/pctmix/constants')
 
 describe('pctmix/crawler/filmCrawler', function () {
 
-    describe('#crawlDataFilm()', function () {
+  describe('#crawlDataFilm()', function () {
      
-        it('should return the film \'Richard dice adios \' with all data', function () {            
-            // https://pctmix1.com/descargar/peliculas-x264-mkv/superdetective-en-hollywood-2-1987-/bluray-microhd/
-            var urlWithFilm=`https://${siteConstants.DOMAIN}/descargar/peliculas-x264-mkv/superdetective-en-hollywood-2-1987-/bluray-microhd/`
+    it('should return the film \'Richard dice adios \' with all data', function () {            
+      // https://pctmix1.com/descargar/peliculas-x264-mkv/superdetective-en-hollywood-2-1987-/bluray-microhd/
+      var urlWithFilm=`https://${siteConstants.DOMAIN}/descargar/peliculas-x264-mkv/superdetective-en-hollywood-2-1987-/bluray-microhd/`
                             
             
-            return filmCrawler.crawlDataFilm(urlWithFilm).then(show => {
-                //console.log("Show crawled:'" + JSON.stringify(show) + "'");
-                assert.equal(show.urlBase, urlWithFilm)
-                assert.equal(show.title, 'Superdetective en hollywood 2')
-                //assert.ok (show.originalTitle)
-                assert.ok(show.sinopsis)
-                assert.ok(show.description)
-                assert.equal(show.quality, 'BluRay 720p X264 MKV')
-                assert.equal(show.fileSize, '2.7 GB')
-                assert.equal(show.releaseDate, '07-07-2021')
-                assert.equal(
-                    show.urlwithCover,
-                    'https://pctmix1.com/pictures/f/mediums/153738_-1625634500-Superdetective-en-Hollywood-2--1987---BluRay-MicroHD.jpg'
-                )
-                assert.equal(show.year, '1987')
-                assert.equal(
-                    show.urltodownload,
-                    'https://pctmix1.com/descargar-torrent/153738_-1625634500-Superdetective-en-Hollywood-2--1987---BluRay-MicroHD'
+      return filmCrawler.crawlDataFilm(urlWithFilm).then(show => {
+        //console.log("Show crawled:'" + JSON.stringify(show) + "'");
+        assert.equal(show.urlBase, urlWithFilm)
+        assert.equal(show.title, 'Superdetective en hollywood 2')
+        //assert.ok (show.originalTitle)
+        assert.ok(show.sinopsis)
+        assert.ok(show.description)
+        assert.equal(show.quality, 'BluRay 720p X264 MKV')
+        assert.equal(show.fileSize, '2.7 GB')
+        assert.equal(show.releaseDate, '07-07-2021')
+        assert.equal(
+          show.urlwithCover,
+          'https://pctmix1.com/pictures/f/mediums/153738_-1625634500-Superdetective-en-Hollywood-2--1987---BluRay-MicroHD.jpg'
+        )
+        assert.equal(show.year, '1987')
+        assert.equal(
+          show.urltodownload,
+          'https://pctmix1.com/descargar-torrent/153738_-1625634500-Superdetective-en-Hollywood-2--1987---BluRay-MicroHD'
                     
-                )
+        )
 
-                //assert.equal(show.originalTitle, '')
-            })
-        })
+        //assert.equal(show.originalTitle, '')
+      })
     })
+  })
 })
