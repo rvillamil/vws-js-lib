175f057de9d5e7ae395ad6a73b3a71a633e6fc09
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2021-07-12 09:58:24 +0200

    All bug fixeds

--- test/dontorrent/searcher/test_showSearcher.js
+++ test/dontorrent/searcher/test_showSearcher.js
@@ -16,17 +16,17 @@ const SearchResultConstants = require('../../../lib/model/searchResultConstants'
 
 describe('dontorrent/searcher/showSearcher', function () {
 
-    describe('#search()', function () {
-        it('should return the first 4 shows for \'Star Wars\' string search (films and TVShows)', function () {
-            //assert.equal(showCollection.shows.length, 6)
-            return showSearcher.search('Star Wars', 4).then(
-                searchResult => {
-                    //console.log(`searchResult found: ${JSON.stringify(searchResult)}`)
-                    assert.equal(searchResult[0].urlWithShow, 'https://dontorrent.org/pelicula/19942/Han-Solo-Una-historia-de-Star-Wars-3D')
-                    assert.equal(searchResult[0].type, SearchResultConstants.FILM)
-                    assert.equal(searchResult.length, 4)
-                }
-            )
-        })
+  describe('#search()', function () {
+    it('should return the first 4 shows for \'Star Wars\' string search (films and TVShows)', function () {
+      //assert.equal(showCollection.shows.length, 6)
+      return showSearcher.search('Star Wars', 4).then(
+        searchResult => {
+          //console.log(`searchResult found: ${JSON.stringify(searchResult)}`)
+          assert.equal(searchResult[0].urlWithShow, 'https://dontorrent.app/pelicula/19942/Han-Solo-Una-historia-de-Star-Wars-3D')
+          assert.equal(searchResult[0].type, SearchResultConstants.FILM)
+          assert.equal(searchResult.length, 4)
+        }
+      )
     })
+  })
 })
