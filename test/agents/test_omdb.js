1a86696f31d9ade2ba53d03d4715ad9303d92106
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2021-07-24 10:48:54 +0200

    fix: Test error

--- test/agents/test_omdb.js
+++ test/agents/test_omdb.js
@@ -13,10 +13,10 @@ describe('omdb', function () {
     it('Should return one Show object with the film Star Wars', function () {
       return omdb.searchShow('Star wars')
         .then(show => {
-          //console.log('Show OMDB: ', show);
+          console.log('Show OMDB: ', show);
           assert.equal(show.title, 'Star wars')
           assert.equal(show.year, '1977')
-          assert.equal(show.originalTitle, 'Star Wars: Episode IV - A New Hope')
+          assert.equal(show.originalTitle, 'Star Wars')
           assert.equal(show.releaseDate, '25 May 1977')
           assert.equal(show.urlwithCover, 'https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg')
           assert.equal(show.error, 0)
