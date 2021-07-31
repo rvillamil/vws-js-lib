cfe994a374db8ca8a1e1444fd97a4c40d6161042
Rodrigo Villamil <rodrigo.villamil@gmail.com>
2021-07-31 16:19:43 +0200

    feat: New function for crawling by location

--- test/test_tvShowCrawler.js
+++ test/test_tvShowCrawler.js
@@ -3,9 +3,6 @@
 /* eslint-disable no-undef */
 const assert = require('assert')
 const crawler = require('../lib/tvShowCrawler')
-// const ShowCollection = require('../lib/model/showCollection')
-// const siteConstants = require('../lib/crawlers/pctmix/constants')
-// const Show = require('../lib/model/show')
 
 describe('tvShowCrawler', function () {
   this.timeout(55000)
@@ -28,31 +25,4 @@ describe('tvShowCrawler', function () {
       },
     ).then((shows) => assert.ok(shows.length === 0)))
   })
-  /*
-  describe('#crawlTVShowCollectionsBy()', () => {
-    it('should return a TVShow collection list with 3 showscollection with 4 episodes every collecion from all torrent sites', () => {
-      const showCollection1 = new ShowCollection()
-      showCollection1.name = 'modern-family/2261'
-      showCollection1.domain = siteConstants.DOMAIN
-      showCollection1.url = `${siteConstants.URL_BASE_TVSHOWS_HD}modern-family/2261`
-
-      const showCollection2 = new ShowCollection()
-      showCollection2.name = 'arrow/1596'
-      showCollection2.domain = siteConstants.DOMAIN
-      showCollection2.url = `${siteConstants.URL_BASE_TVSHOWS_HD}arrow/1596`
-      const showCollectionList = []
-      showCollectionList.push(showCollection1)
-      showCollectionList.push(showCollection2)
-      //    showCollectionList.push(showCollection3)
-
-      return crawler.crawlTVShowCollectionsBy(4, showCollectionList)
-        .then((newShowCollectionList) => {
-          assert.equal(newShowCollectionList.length, 2)
-          assert.equal(newShowCollectionList[0].shows.length, 4)
-          assert.equal(newShowCollectionList[1].shows.length, 4)
-          //      assert.equal(newShowCollectionList[2].shows.length, 4)
-        })
-    })
-  })
-  */
 })
